223a8930df0117642b4f1c6103bf29be357a64ab
vitalij99 <vitalisklymko@gmail.com>
2023-05-05 22:49:41 +0300

    add Select

--- src/pages/Tweets/Tweets.jsx
+++ src/pages/Tweets/Tweets.jsx
@@ -1,8 +1,21 @@
+import Select from "react-select";
 import UserList from "../../components/UserList/UserList";
 import "./Tweets.scss";
+import { customStyles, options } from "../../components/Dropdown/Dropdown";
+import { useState } from "react";
 const Tweets = () => {
+    const [selectedOption, setSelectedOption] = useState(null);
+
+    const handleChange = ({ value }) => {
+        setSelectedOption(value);
+    };
     return (
         <>
+            <Select
+                onChange={handleChange}
+                options={options}
+                styles={customStyles}
+            />
             <div className="wrapper">
                 <UserList />
             </div>
