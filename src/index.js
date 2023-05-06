26b0ec5bc868991fc9406d547fb4b3ada1617279
vitalij99 <vitalisklymko@gmail.com>
2023-05-06 17:10:02 +0300

    add basename and 404 html

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <BrowserRouter>
+        <BrowserRouter basename="goit-tweets-task">
             <Provider store={store}>
                 <PersistGate loading={null} persistor={persistor}>
                     <App />
